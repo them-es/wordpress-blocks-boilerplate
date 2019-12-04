3446b68a49fbb3dd0f6becdc026663be2b796746
them-es <them-es@users.noreply.github.com>
2019-12-04 10:26:39 +0100

    Automate Dependency Management using index.asset.php

--- index.php
+++ index.php
@@ -74,28 +74,28 @@ add_filter( 'block_categories', 'wordpress_blocks_starter_block_categories', 10,
 
 /**
  * Enqueue block assets: Backend
+ * 
+ * https://github.com/WordPress/gutenberg/blob/master/docs/designers-developers/developers/tutorials/javascript/js-build-setup.md#dependency-management
  */
 
 function wordpress_blocks_starter_enqueue_block_editor_assets() {
-	$block_file = 'blocks/build/index.js';
+	$blocks_dir = 'blocks/build/';
+
+	$blocks_asset_file = include get_theme_file_path( $blocks_dir . 'index.asset.php' );
+
+	// Replace "wp-blockEditor" with "wp-block-editor"
+	$blocks_asset_file['dependencies'] = array_replace( $blocks_asset_file['dependencies'],
+		array_fill_keys(
+			array_keys( $blocks_asset_file['dependencies'], 'wp-blockEditor' ),
+			'wp-block-editor'
+		)
+	);
 
 	wp_enqueue_script(
 		'wordpress-blocks-starter-blocks',
-		get_theme_file_uri( $block_file, __FILE__ ),
-		array( 'wp-blocks', 'wp-components', 'wp-editor', 'wp-i18n' ),
-		filemtime( get_theme_file_path( $block_file ) )
+		get_theme_file_uri( $blocks_dir . 'index.js', __FILE__ ),
+		$blocks_asset_file['dependencies'],
+		$blocks_asset_file['version']
 	);
 }
 add_action( 'enqueue_block_editor_assets', 'wordpress_blocks_starter_enqueue_block_editor_assets' );
-
-
-/**
- * Enqueue block assets: Frontend
- */
-
-/*
-function wordpress_blocks_starter_enqueue_block_assets() {
-	
-}
-add_action( 'enqueue_block_assets', 'wordpress_blocks_starter_enqueue_block_assets' );
-*/
