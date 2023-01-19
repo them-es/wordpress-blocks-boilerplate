11ba396be28a91e474c4e1a84c0c6a0ec429e7b7
them-es <them-es@users.noreply.github.com>
2023-01-19 11:06:28 +0100

    Documentation: Add PHPDoc variables

--- index.php
+++ index.php
@@ -2,16 +2,20 @@
 /**
  * Add custom block category to default categories.
  *
- * https://wordpress.org/gutenberg/handbook/designers-developers/developers/filters/block-filters/#managing-block-categories
+ * @param array[]                 $block_categories     Block categories.
+ * @param WP_Block_Editor_Context $block_editor_context Block Editor context.
+ *
+ * @return array
  */
-function wordpress_blocks_starter_block_categories( $categories, $post ) {
+function wordpress_blocks_starter_block_categories( $block_categories, $block_editor_context ) {
 	/*
-	if ( $post->post_type !== 'post' ) {
-		return $categories;
+	if ( 'post' !== $block_editor_context->post->post_type ) {
+		return $block_categories;
 	}
 	*/
+
 	return array_merge(
-		$categories,
+		$block_categories,
 		array(
 			array(
 				'slug'  => 'theme-blocks',
@@ -22,11 +26,11 @@ function wordpress_blocks_starter_block_categories( $categories, $post ) {
 }
 add_filter( 'block_categories_all', 'wordpress_blocks_starter_block_categories', 10, 2 );
 
-
 /**
  * Enqueue block assets: Backend.
- *
  * https://github.com/WordPress/gutenberg/blob/master/docs/designers-developers/developers/tutorials/javascript/js-build-setup.md#dependency-management
+ *
+ * @return void
  */
 function wordpress_blocks_starter_enqueue_block_editor_assets() {
 	$blocks_dir = basename( __DIR__ ) . '/build/';
@@ -37,7 +41,8 @@ function wordpress_blocks_starter_enqueue_block_editor_assets() {
 		'wordpress-blocks-starter-blocks',
 		get_theme_file_uri( $blocks_dir . 'index.js' ), // Plugin path: plugin_dir_url( dirname( __FILE__ ) ) . $blocks_dir . '/index.js',
 		$blocks_asset_file['dependencies'],
-		$blocks_asset_file['version']
+		$blocks_asset_file['version'],
+		true
 	);
 }
 add_action( 'enqueue_block_editor_assets', 'wordpress_blocks_starter_enqueue_block_editor_assets' );
